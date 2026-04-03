dcc1965115e302f14c4c7366ebbd833feccf7fa4
Chandini <skokila624@gmail.com>
2026-04-03 20:43:02 +0530

    Update user role enum in user model

--- models/userModel.js
+++ models/userModel.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true }, // <--- required
   password: { type: String, required: true },
-  role: { type: String, enum: ['super_admin', 'faculty', 'student', 'researcher'] },
+  role: { type: String, enum: ['super_admin', 'faculty', 'student', 'admin', 'Finance Department', 'University of Management', 'Director-R&D', 'Assistant Director-Research'] },
   department: String,
   createdAt: { type: Date, default: Date.now }
 });
